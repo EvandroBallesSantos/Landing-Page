dd76329e4f897664c72a53819f3ab0aa6fffe6b0
EvandroBallesSantos <evandro_ebs@yahoo.com.br>
2025-05-14 01:11:38 -0300

    feat: atualização no projeto otimizando, refatorando e estilizando.

--- main.js
+++ main.js
@@ -1,37 +1,3 @@
-var cabecalho = document.getElementById("cabecalho");
-var menuLista = document.getElementById("menu-lista");
-var conteudo = document.getElementById("conteudo");
-var showSidebar = false;
-
-function tooggleSidebar() {
-  showSidebar = !showSidebar;
-    if(showSidebar) {
-        menuLista.style.marginTop = '0vw';
-        menuLista.style.animationName = 'showSiderbar';
-        conteudo.style.filter = 'blur(5px)';
-    } else {
-        menuLista.style.marginTop = '';
-        menuLista.style.animationName = 'showSiderbarInvert';
-        conteudo.style.filter = 'blur(0px)';
-    }
-}
-
-function closeSidebar() {
-    if(showSidebar){
-        tooggleSidebar();
-    }else {
-        (!showSidebar)
-    }
-}
-
-window.addEventListener('resize', function(event) {
-    if(this.window.innerWidth > 768 && showSidebar) {
-        tooggleSidebar();
-    }else {
-        (closeSidebar)
-    }
-});
-
 
 /* Efeito deslizar página toda */
 
@@ -45,18 +11,47 @@ const myObserver = new IntersectionObserver((entries) => {
     })
 })
 
+// Section 1
 const content01 = document.querySelectorAll('.conteudo-principal-1-escrito');
-const content02 = document.querySelectorAll('.div-img');
+const content011 = document.querySelectorAll('.conteudo-principal-1-ola');
+const content012 = document.querySelectorAll('.conteudo-principal-1-nome');
+const content013 = document.querySelectorAll('.conteudo-principal-1-decript');
+const content014 = document.querySelectorAll('.lista-contato');
+const content015 = document.querySelectorAll('.div-img');
+
+// section 2
+const content02 = document.querySelectorAll('.titulo-quem-sou');
+const content022 = document.querySelectorAll('.descricao-quem-sou');
+const content023 = document.querySelectorAll('.description-tecnologias');
+const content024 = document.querySelectorAll('.description-cursos');
+
+
 const content03 = document.querySelectorAll('.container-quem-sou');
-const content04 = document.querySelectorAll('.img-img');
 const content05 = document.querySelectorAll('.portifolio');
 const content06 = document.querySelectorAll('.container-cards');
 const content07 = document.querySelectorAll('.contato');
 
+const content08 = document.querySelectorAll('footer');
+
+// Section 1
 content01.forEach((element) => myObserver.observe(element))
+content011.forEach((element) => myObserver.observe(element))
+content012.forEach((element) => myObserver.observe(element))
+content013.forEach((element) => myObserver.observe(element))
+content014.forEach((element) => myObserver.observe(element))
+content015.forEach((element) => myObserver.observe(element))
+
+// Section 2
 content02.forEach((element) => myObserver.observe(element))
+content022.forEach((element) => myObserver.observe(element))
+content023.forEach((element) => myObserver.observe(element))
+content024.forEach((element) => myObserver.observe(element))
+
+
+
+
 content03.forEach((element) => myObserver.observe(element))
-content04.forEach((element) => myObserver.observe(element))
 content05.forEach((element) => myObserver.observe(element))
 content06.forEach((element) => myObserver.observe(element))
 content07.forEach((element) => myObserver.observe(element))
+content08.forEach((element) => myObserver.observe(element))
