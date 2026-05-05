df5071cdc400b1cc2fd6237ecba44ada54f78e1b
EvandroBallesSantos <evandro_ebs@yahoo.com.br>
2026-05-05 11:24:11 -0300

    refactor: eliminando código repetitivo.

--- main.js
+++ main.js
@@ -1,69 +1,38 @@
-/* Efeito deslizar página toda */
+const selectors = [
+    '.slide-in',
+    '.fade-in',
+    '.div-img',
+    '.conteudo-principal-1-nome',
+    '.descTitles',
+    '.conteudo-principal-1-descript',
+    '.miniCardsSkills',
+    '.containerButtons',
+    '.titulo-quem-sou',
+    '.descricao-quem-sou',
+    '.description-tecnologias',
+    '.cardsSkills',
+    '.portifolioAll',
+    '.card-only',
+    '.lista-contato',
+    '.limitar'
+];
 
-const myObserver = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if(entry.isIntersecting){
-            entry.target.classList.add('show')
-        } else {
-            entry.target.classList.remove('show')
+const observerOptions = {
+    root: null,
+    rootMargin: '0px 0px -10% 0px',
+    threshold: 0
+};
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach( ( entry ) => {
+        if ( entry.isIntersecting ) {
+            entry.target.classList.add( 'show' );
+    } else {
+      entry.target.classList.remove('show');
         }
-    })
-})
+    } );
+}, observerOptions);
 
-// Section 1
-const content01 = document.querySelectorAll('.conteudo-principal-1-escrito');
-const content011 = document.querySelectorAll('.descTitles');
-const content012 = document.querySelectorAll('.conteudo-principal-1-nome');
-const content013 = document.querySelectorAll('.conteudo-principal-1-descript');
-const content014 = document.querySelectorAll('.lista-contato');
-const content015 = document.querySelectorAll('.div-img');
-const content016 = document.querySelectorAll('.miniCardsSkills');
-const content017 = document.querySelectorAll('.containerButtons');
-
-// section 2 QUEM SOU EU
-const content02 = document.querySelectorAll('.titulo-quem-sou');
-const content022 = document.querySelectorAll('.descricao-quem-sou');
-const content023 = document.querySelectorAll('.description-tecnologias');
-const content024 = document.querySelectorAll('.description-cursos');
-const content025 = document.querySelectorAll('.button-cv');
-const content026 = document.querySelectorAll('.description-tecnologias');
-const content027 = document.querySelectorAll('.cardsSkills');
-
-
-const content03 = document.querySelectorAll('.container-quem-sou');
-const content05 = document.querySelectorAll('.portifolioAll');
-const content06 = document.querySelectorAll('.container-cards');
-const content07 = document.querySelectorAll('.card-only');
-const content08 = document.querySelectorAll('.limitar');
-
-
-const content09 = document.querySelectorAll('.rodape');
-
-// Section 1
-content01.forEach((element) => myObserver.observe(element))
-content011.forEach((element) => myObserver.observe(element))
-content012.forEach((element) => myObserver.observe(element))
-content013.forEach((element) => myObserver.observe(element))
-content014.forEach((element) => myObserver.observe(element))
-content015.forEach((element) => myObserver.observe(element))
-content016.forEach((element) => myObserver.observe(element))
-content017.forEach((element) => myObserver.observe(element))
-
-// Section 2
-content02.forEach((element) => myObserver.observe(element))
-content022.forEach((element) => myObserver.observe(element))
-content023.forEach((element) => myObserver.observe(element))
-content024.forEach((element) => myObserver.observe(element))
-content025.forEach((element) => myObserver.observe(element))
-content026.forEach((element) => myObserver.observe(element))
-content027.forEach((element) => myObserver.observe(element))
-
-
-
-
-content03.forEach((element) => myObserver.observe(element))
-content05.forEach((element) => myObserver.observe(element))
-content06.forEach((element) => myObserver.observe(element))
-content07.forEach((element) => myObserver.observe(element))
-content08.forEach((element) => myObserver.observe(element))
-content09.forEach((element) => myObserver.observe(element))
+document.querySelectorAll( selectors.join( ', ' ) ).forEach( ( el ) => {
+    observer.observe( el );
+});
